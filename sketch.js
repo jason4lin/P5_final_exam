dfc62b183fe48b6f6ae6fe66447bf7f61e549003
jason4lin <jasonlin.lcx@gmail.com>
2024-01-05 01:09:42 +0800

    API

--- sketch.js
+++ sketch.js
@@ -1,15 +1,90 @@
-import steamAPI from "./steamAPI.js";
-const steamAPI = new steamAPI
+const steamAPI = new steamAPI_
+
+//總頁數
+const pages = 6;
+//當前頁面
+var nowPage = 0;
+//上次切換時間
+var lastTime = 0;
 
 function setup() {
-  createCanvas(400, 400);
-  var steamAPI = new steamAPI()
-  const list = steamAPI.getOwnedGames();
-  console.log(list);
+  createCanvas(windowWidth, windowHeight);
+  textSize(100)
+  getSteamInv()
 }
 
 function draw() {
-  background(220);
+  background(100)
+  switch(nowPage){
+    case 1:
+      text(nowPage,width/2,height/2)
 
+    case 2:
+      text(nowPage,width/2,height/2)
+
+    case 3:
+      text(nowPage,width/2,height/2)
+
+    case 4:
+      text(nowPage,width/2,height/2)
+
+    case 5:
+      text(nowPage,width/2,height/2)
+
+    case 6:
+      text(nowPage,width/2,height/2)
+
+    default:
+  }
 }
 
+//滑鼠點一下換一頁
+function mouseClicked() {
+  let nowTime = Date.now();
+  //1秒只能點一下
+  if (nowTime > lastTime+1000){
+    lastTime = nowTime;
+    //頁數改變
+    if (nowPage < pages){
+      nowPage++;
+    } else {
+      nowPage =0;
+    }
+  }
+}
+
+async function getSteamInv() {
+  const list = (await steamAPI.getOwnedGames()).response.games;
+
+  
+  for (let index = 0; index < 5; index++) {
+    const e = list[index];
+    const id = e.appid
+
+    const response = await steamAPI.appDetails(id);
+
+    if (response && response[id] && response[id].success) {
+      const appStoreInfo = response[id].data;
+      const tag = []
+      appStoreInfo.categories.forEach(e => {
+        tag.push(e.description)
+      });
+      appStoreInfo.genres.forEach(e => {
+        tag.push(e.description)
+      });
+
+      const newE = {...e, 
+        name:appStoreInfo.name, 
+        initPrice:appStoreInfo.price_overview.initial_formatted, 
+        lowestPrice:appStoreInfo.price_overview.final_formatted,
+        tag:tag
+      }
+      list[index]=newE
+      console.log(list[index])
+      console.log(list[index].tag)
+    }
+    setTimeout(() => {}, 1000);
+  }
+  console.log(list)
+
+}
--- steamAPI.js
+++ steamAPI.js
@@ -2,16 +2,22 @@ class sendRequest {
   constructor() {
     this.key = "7A1DF9F12E6E73FF0B11DAF38666C1B8";
     this.steamID = "76561198122456037";
-    this.retryDelay = 100;
+    this.retryDelay = 1000;
 
     this.optionsget = {
       method: "get",
-      headers: {},
+      headers: {
+        "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      },
     };
 
     this.optionspost = {
       method: "post",
-      headers: {},
+      headers: {
+        "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      },
     };
   }
 
@@ -62,7 +68,7 @@ class sendRequest {
   }
 }
 
-class steamAPI extends sendRequest {
+class steamAPI_ extends sendRequest {
   constructor(token, retryDelay) {
     super(token, retryDelay);
     //this.token = token;
@@ -84,7 +90,7 @@ class steamAPI extends sendRequest {
   appDetails(appid) {
     const options = this.optionsget;
     return super
-      .fetchDataWithRetry(`https://store.steampowered.com/api/appdetails?appids=${appid}`, options)
+      .fetchDataWithRetry(`https://store.steampowered.com/api/appdetails?language=tchinese&appids=${appid}`, options)
       .then((data) => {
         return data;
       })
@@ -94,34 +100,45 @@ class steamAPI extends sendRequest {
       });
   }
 
-  getItemID() {
-    return super
-      .fetchDataWithRetry(
-        "https://api.swordgale.online/api/items",
-        this.optionsget
-      )
-      .then((data) => {
-        let items = data.mines;
-        return items;
-      })
-      .catch((error) => {
-        console.error("fetchDataWithRetry Error:", error.message);
-        throw error;
-      });
-  }
 }
 
-var _steamAPI = new steamAPI()
-var games = [];
+// var _steamAPI = new steamAPI()
 
-async function main() {
-    const list = (await _steamAPI.getOwnedGames()).response.games;
-    list.forEach(element,index => {
-        const gameinfo = _steamAPI.appDetails(element.appid)
-        list[index].push(gameinfo)
-    });
-    console.log(list);
-}
+// async function main() {
+//     const list = (await _steamAPI.getOwnedGames()).response.games;
 
-main();
+    
+//     for (let index = 0; index < 5; index++) {
+//       const e = list[index];
+//       const id = e.appid
+
+//       const response = await _steamAPI.appDetails(id);
+
+//       if (response && response[id] && response[id].success) {
+//         const appStoreInfo = response[id].data;
+//         const tag = []
+//         appStoreInfo.categories.forEach(e => {
+//           tag.push(e.description)
+//         });
+//         appStoreInfo.genres.forEach(e => {
+//           tag.push(e.description)
+//         });
+  
+//         const newE = {...e, 
+//           name:appStoreInfo.name, 
+//           initPrice:appStoreInfo.price_overview.initial_formatted, 
+//           lowestPrice:appStoreInfo.price_overview.final_formatted,
+//           tag:tag
+//         }
+//         list[index]=newE
+//         console.log(list[index])
+//         console.log(list[index].tag)
+//       }
+//       setTimeout(() => {}, 1000);
+//     }
+//     console.log(list)
+
+// }
+
+// main();
 
